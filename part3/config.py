b9f5d23285213fb4c47e9c4027e0a0c0d29c0cba
root <root@Elyes.>
2025-03-28 16:33:21 +0000

    updated config file with development settings enabled

--- part3/config.py
+++ part3/config.py
@@ -1,13 +1,23 @@
 import os
 
 class Config:
+    """Base configuration."""
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
+    """Development configuration."""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_dev.db'
+
+class ProductionConfig(Config):
+    """Production configuration."""
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://user:password@localhost/hbnb_prod')
+
 
 config = {
     'development': DevelopmentConfig,
+    'production': ProductionConfig,
     'default': DevelopmentConfig
 }
