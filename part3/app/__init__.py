3a653f8a621115df333307dcf610ed00b3a63bcc
root <root@Elyes.>
2025-03-29 16:47:41 +0000

    configuration of jwt manager

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -10,6 +11,7 @@ from app.api.v1.reviews import api as reviews_ns
 from app.config import Config
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
