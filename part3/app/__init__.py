2c372cfd07adf194694fe3bf7f531b3dd870a4b4
root <root@Elyes.>
2025-03-29 16:50:16 +0000

    registered jwt manager

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -18,7 +18,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
 
     bcrypt.init_app(app)
-
+    jwt.init_app(app)
+    
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     
     api.add_namespace(users_ns, path='/api/v1/users')
