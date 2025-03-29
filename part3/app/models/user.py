25dc01e03e01876867dbb8435a7f703bd96186b4
root <root@Elyes.>
2025-03-29 15:47:55 +0000

    updated user model to hash and verify pw

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -13,3 +13,11 @@ class User(BaseModel):
         """Validates email format."""
         pattern = r"[^@]+@[^@]+\.[^@]+"
         return re.match(pattern, email) is not None
+
+    def hash_password(self, password):
+    """Hashes the password before storing it."""
+    self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+    """Verifies if the provided password matches the hashed password."""
+    return bcrypt.check_password_hash(self.password, password)
